be8192cf0d4ef0b5aba374013f99714821a5015f
GustavoFFLima <frazao39@gmail.com>
2022-10-28 17:41:17 -0300

    busca mensagens automatizadas

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let usuario = {};
-let mensagens = {};
+let mensagens = [];
+let mensagensTratadas = [];
+const sectionMSG = document.querySelector('.sectionMSG');
 
 function nomeUsuario() {
     let nome = "Digite seu nome"
@@ -19,10 +21,48 @@ function processarMensagens() {
 };
 
 function recebeMensagens(res) {
-    mensagens = res.data
+    mensagens = res.data;
+    renderizandoMensagem();
 };
 
 setInterval(function () {
     axios
         .post('https://mock-api.driven.com.br/api/v6/uol/status', usuario)
 }, 5000)
+
+function gerandoMensagem(mensagens) {
+    const mensagemPostada = document.createElement('div');
+    const tempo = document.createElement('div');
+    const tempoP = document.createElement('p'); 
+    const status = document.createElement('div');
+    const statusP = document.createElement('p'); 
+    const texto = document.createElement('div');
+    const textoP = document.createElement('p'); 
+
+    mensagemPostada.className = 'mensagemPostada';
+    tempo.className = 'tempo'; 
+    status.className = 'status'; 
+    texto.className = 'texto'; 
+
+    tempoP.innerHTML = mensagens.time;
+    statusP.innerHTML = `${mensagens.from} para ${mensagens.to}:`;
+    textoP.innerHTML = mensagens.text;
+
+    tempo.appendChild(tempoP);
+    status.appendChild(statusP);
+    texto.appendChild(textoP);
+    mensagemPostada.appendChild(tempo);
+    mensagemPostada.appendChild(status);
+    mensagemPostada.appendChild(texto);
+
+    return mensagemPostada;
+};
+
+function renderizandoMensagem() {
+    
+    mensagens.forEach(function(mensagens) {
+        const mensagemRenderizada = gerandoMensagem(mensagens);
+        sectionMSG.appendChild(mensagemRenderizada);
+    });
+};
+renderizandoMensagem() 
